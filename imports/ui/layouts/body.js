51d94fdadf088cb77b4b5c5ea3dc35f1568ea07a
Oyetoke Toby <oyetoketoby80@gmail.com>
2018-11-03 21:39:49 +0100

    Push

--- imports/ui/layouts/body.js
+++ imports/ui/layouts/body.js
@@ -7,20 +7,28 @@ import { PriceRegisters } from '../../api/collections.js';
 import { Stores } from '../../api/collections.js';
 import { Wishlists } from '../../api/collections.js';
 import '../../api/methods.js'
+import '../css/main.css'
 
 import '../pages/About.html';
 import '../pages/Account.html';
+import '../pages/SignUp.html';
+import '../pages/SignUp2.html';
 import '../pages/Dashboard.html';
 import '../pages/Ecommerce.html';
 import '../pages/Enroll.html';
 import '../pages/Home.html';
+import '../pages/HomeLoggedIn.html';
 import '../pages/Invite.html';
+import '../pages/Help.html';
 import '../pages/Privacy.html';
 import '../pages/Profile.html';
 import '../pages/Support.html';
 import '../pages/Wishlist.html';
 
 import '../partials/AboutUs.html';
+import '../partials/HeaderCenter.html';
+import '../partials/Header.html';
+import '../partials/PopUpNav.html';
 import '../partials/BoxArea.html';
 import '../partials/ChangePasswordForm.html';
 import '../partials/Confirmation.html';
@@ -32,12 +40,15 @@ import '../partials/Footer.html';
 import '../partials/ForgotPasswordForm.html';
 import '../partials/IllustrationBox.html';
 import '../partials/InviteForm.html';
+import '../partials/HelpForm.html';
 import '../partials/Jumbotron.html';
 import '../partials/LoginForm.html';
 import '../partials/PrivacyForm.html';
 import '../partials/ProfileForm.html';
 import '../partials/SideMenu.html';
 import '../partials/SignUpForm.html';
+import '../partials/SignUp2Form.html';
+import '../partials/ProductsPopUp.html';
 import '../partials/Wish.html';
 import '../partials/WishBox.html';
 import '../partials/WishDetails.html';
